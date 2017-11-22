261eb380d4a83b3bc57d9a8a37fd4036ef35a14e
alphaeye <alphaeye@aliyun.com>
2017-11-22 16:52:13 +0800

    build_file

---
+++ tour/build_file.php
@@ -0,0 +1,32 @@
+<?php
+
+$prefix = '03';
+$range = range(5, 27);
+
+var_dump($range);
+echo PHP_EOL;
+foreach ($range as $key => $value) {
+    $len = strlen($value);
+    if ($len == 1) {
+      $filename = $prefix.'.0'.$value;
+      $lastname = $prefix.'.0'.($value-1);
+      $nextname = $prefix.'.0'.($value+1);
+    } else {
+      $filename = $prefix.'.'.$value;
+      $lastname = $prefix.'.'.($value-1);
+      $nextname = $prefix.'.'.($value+1);
+    }
+    echo $filename.PHP_EOL;
+
+    $content = "## $filename （）
+
+
+## 链接
+* [目录](https://github.com/alpha2018/go-zh/blob/master/tour/directory.md)
+* 上一节：[$lastname （）](https://github.com/alpha2018/go-zh/blob/master/tour/$lastname.md)
+* 下一节：[$nextname （）](https://github.com/alpha2018/go-zh/blob/master/tour/$nextname.md)
+";
+
+      file_put_contents($filename.'.md', $content);
+
+}
