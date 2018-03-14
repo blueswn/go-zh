7ffd42166ddb0def332c2a21544516e2e3274588
alpha2018 <alpha2018@aliyun.com>
2018-03-14 15:10:37 +0800

    doc

---
+++ tour/read_title.php
@@ -0,0 +1,19 @@
+<?php
+
+$list = scandir('../tour/');
+
+$titles = [];
+foreach ($list as $filename) {
+    echo "$filename\n";
+    if (substr($filename, 0,2) == '03') {
+      $content = file_get_contents('./'.$filename);
+      $contents = explode(PHP_EOL, $content);
+      $title = str_replace('## ', '  * [', $contents[0]);
+      $title = $title.'](https://github.com/alpha2018/go-zh/blob/master/tour/'.$filename.')';
+      $titles[] = $title;
+
+    }
+}
+
+$titlesStr = implode(PHP_EOL, $titles);
+echo $titlesStr;
